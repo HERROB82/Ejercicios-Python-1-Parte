00660650ef3a0143d77c98a5e6c5ea64093e055b
HERROB82 <hrz.salemme@gmail.com>
2023-01-18 20:19:56 +0100

    POO M3.4

--- POO/M3_Practicando.py
+++ POO/M3_Practicando.py
@@ -253,7 +253,7 @@ print(type(p_alien.duracion))
 print(type(p_alien.lanzamiento))
 print(type(p_alien.director))'''
 
-# Algo mas AVANZADO: Creando un catalogo de peliculas (LISTA DE OBJETOS)
+# Algo mas AVANZADO: Creando un catalogo de peliculas (LISTA DE OBJETOS) #### REPASAR
 
 # Retomamos la pelicula original
 class Pelicula():
@@ -263,7 +263,7 @@ class Pelicula():
     self.titulo = titulo
     self.duracion = duracion
     self.lanzamiento = lanzamiento
-    print("Se ha creado la pelicula",self.titulo)
+    print("Se ha creado la pelicula: ",self.titulo)
 
   def __str__(self):
     return "{} ({})".format(self.titulo,self.lanzamiento)
@@ -272,18 +272,66 @@ class Catalogo():
 
   peliculas = [] #Esta lista contendra objetos de la clase Pelicula
   #El valor por defecto del catalogo es peliculas = [] (xq todavia no hay ninguna peliculas y esta vacio)
-  def __init__(self,peliculas[]):
+  def __init__(self,peliculas=[]):
     self.peliculas = peliculas
-    if (len(peliculas == 0)):
-      print("Se ha creado el catalogo vacio")
+    if (len(self.peliculas) == 0):
+        print("Se ha creado el catalogo vacio")
     else:
-      print("Se ha creado el catalogo con  las peliculas asignadas")
+        print("Se ha creado el catalogo con  las peliculas asignadas")
   
   # Toda la interaccion que hagamos conlas variables de una clase (es decir; modificacion, lectura, lo que sea) con los 
   # atributos de una clase SIEMPRE DEBERIA SER A TRAVES DE UN METODO!!
   def agregar(self,p):#x eso tengo aqui el metodo agregar que recibe un parametro (p) que sera un objeto de la clase pelicula
     self.peliculas.append(p) #le hacemos un append a "peliculas" xa ir agregando 
     print("Se ha agregado al catalogo la pelicula: {}".format(p.titulo)) #VER ESTO!!!!
+
+  def mostrar(self):
+    for p in self.peliculas:
+      print(p) #Print toma por defecto str(p)
+
+# Ahora creamos los objetos peliculas
+p1 = Pelicula("El Padrino",175,1972)
+p2 = Pelicula("Alien el octavo pasajero",200,1979)
+listaPeliculas=[p1,p2] #creamos una variable de listaPeliculas para verlo mas claro
+print(type(listaPeliculas))
+
+c = Catalogo(listaPeliculas) #creamos el objeto catalogo con dos peliculas que tb tenemos creadas
+  
+c.mostrar() #esta itera sobre la lista de objetos que existe en "c"
+
+# Con esto añadimos una nueva pelicula directamente
+c.agregar(Pelicula("El Padrino: Parte 2",202,1974))
+
+# Alternativa a la linea anterior
+p3 = Pelicula("El Padrino: Parte 3",202,1980)
+c.agregar(p3) # "c" es el objeto del catagolo y le invoco el metodo "agregar" y le paso el objeto "p3"
+
+for peli in c.peliculas:
+  print(peli.lanzamiento) #aqui ya voy poniendo .lanzamiento o .lo que quiera xa ver solo otro dato
+
+c2 = Catalogo()
+c2.mostrar()
+
+# Creamos todo junto
+pM1=Pelicula("Matrix",175,1999)
+pM2=Pelicula("Matrix Reload",175,2003)
+pM3=Pelicula("Matrix Revolution",175,2005)
+c3 = Catalogo ([pM1,pM2,pM3])
+
+c3.mostrar()
+
+
+## CLASE Nº 4 ##
+
+# Este comienza diciendo que no es herencia y que el ejercicio 5 de esta unidad es lo mismo que el ejercicio de películas 
+# que van dentro de un catálogo, un pedido se compone varios productos (que vamos a ir metiendo) o unos productos van dentro 
+# de pedido.
+
+# Lo que da juego es una lista de objetos y nos habíamos quedado en el método de c.mostrar() que lo que hace es preguntarle
+# al objeto c, qué tal si me listas tu lista de películas?, ya que la visualización no es simple sino que hay que visualizar
+# una lista y para verlo esta utilizando un bucle for:
+  
+
 
   
 
